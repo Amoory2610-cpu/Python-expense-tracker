71a2f1d3f434d8006e0bdbd7d1aff4e0519425c6
ofehm <ofehmy@gmail.com>
2026-03-23 09:08:43 +0000

    my project thus far

--- expense_manager.py
+++ expense_manager.py
@@ -1,8 +1,31 @@
 import csv
 
 def add_expense(amount, category):
-    with open("data/expenses.csv", mode="a", newline="") as file:
-        writer = csv.writer(file)
+    with open("./data/expenses.csv", mode="a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([amount, category])
 
+
+def view_expenses():
+    with open("./data/expenses.csv", mode="r") as file:
+        reader = csv.reader(file)
+
+        next(reader)
+
+        for row in reader:
+            print(f"Amount: {row[0]} | Category: {row[1]}")
+
+
+def total_expenses():
+    total = 0
+
+    with open("./data/expenses.csv", mode="r") as file:
+        reader = csv.reader(file)
+
+        next(reader)
+
+        for row in reader:
+            amount = float(row[0])
+            total += amount
+
+    print("Total expenses:", total)
--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from expense_manager import add_expense
+from expense_manager import add_expense, view_expenses, total_expenses
 
 def main():
-    amount = float(input("Enter amount: "))
-    category = input("Enter category: ")
-    add_expense(amount, category)
-    print ("Saved successfully!")
+    while True:
+        print("1. Add Expense")
+        print("2. View expense")
+        print("3. total_expenses")
+        print("4. exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            amount = float(input("Enter amount:"))
+            category = input("Enter category: ")
+            add_expense(amount, category)
+
+
+        elif choice == "2":
+            view_expenses()
+
+        elif choice == "4":
+            total_expenses()
+
+
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice\n")
+
+if __name__ == "__main__":
+    main()
